1c6b16d9810186637af0871341bb209384701a90
Giovanny <giovannydiaz98@yahoo.com>
2019-04-21 02:21:33 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -14,9 +14,8 @@ function ageChecker(age) {
     return "You are a teenager!";
       }else if (age <= 12);{
        return "You are a kid";}
-         }else{
+         } else if (age > 19){
      return "You are a grownup";}
-}
 
 function ternaryTeenager(age) {
 return(age >= 13 && age <= 19) ? "You are a teenager" : "You are not a teenager";
